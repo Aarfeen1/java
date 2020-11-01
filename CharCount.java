8f3a62d0285632d6f44e22a093dd0f9c956583d1
Aarfeen1 <72484730+Aarfeen1@users.noreply.github.com>
2020-11-01 23:47:54 +0530

    Add files via upload

---
+++ CharCount.java
@@ -0,0 +1,24 @@
+
+public class CharCount{
+
+	public static void main(String[] args) {
+		
+		String str = "hheeeeellllloooo" ;
+		char c = 'e';
+		System.out.println("Number of times Character "+ c + " is repeated : " + count(str,c));
+		
+	}
+	
+	public static int count(String str , char c) {
+		int coun = 0;
+		for(int i = 0; i<  str.length() ; i++) {
+			if (str.charAt(i) == c)
+				coun++;
+		}
+		return coun;
+		
+	}
+	
+	
+}
+
