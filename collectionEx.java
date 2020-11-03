beb8f07c0705b0f330f250cddfe58667fbdd9424
Aarfeen1 <72484730+Aarfeen1@users.noreply.github.com>
2020-11-03 06:57:30 +0530

    Add files via upload

---
+++ collectionEx.java
@@ -0,0 +1,18 @@
+package collectionEx;
+
+import java.util.ArrayList;
+
+ class collectionEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ArrayList<Object> valuecol=new ArrayList<>();
+valuecol.add("name");
+valuecol.add(10);
+valuecol.add(false);
+valuecol.add(10.00);
+valuecol.add(null);
+System.out.println(valuecol);
+	}
+
+}
