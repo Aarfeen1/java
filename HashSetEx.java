e57212aca32ab471ee7df23652a96e7ea4fd6bd3
Aarfeen1 <72484730+Aarfeen1@users.noreply.github.com>
2020-12-01 07:26:08 +0530

    Add files via upload

---
+++ HashSetEx.java
@@ -0,0 +1,16 @@
+package Set;
+
+import java.util.HashSet;
+
+public class HashSetEx {
+
+	public static void main(String[] args) {
+	HashSet<String> h =new HashSet<String>();
+	h.add("b");
+	
+	
+	
+
+	}
+
+}
---
+++ SetEx.java
@@ -0,0 +1,11 @@
+package Set;
+
+import java.util.HashSet;
+
+public class SetEx {
+	
+HashSet<String> names=new HashSet<String>();
+names.add("kallma");
+names.add("salim");
+
+}
